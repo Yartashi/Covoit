cc4132aba7146a8c9d2dd666c3cf6f397efb9cc3
Yartashi <58363953+Yartashi@users.noreply.github.com>
2020-05-16 14:33:56 +0200

    trajet CRUD

--- src/Controller/TrajetController.php
+++ src/Controller/TrajetController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 use App\Entity\Trajet;
+use App\Form\TrajetType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,7 +23,7 @@ class TrajetController extends AbstractController
 
     /**
      * Lister tous les trajets.
-     * @Route("/trajet/", name="trajet.list")
+     * @Route("/trajet", name="trajet.list")
      * @return Response
      */
     public function list() : Response
@@ -29,5 +32,27 @@ class TrajetController extends AbstractController
     return $this->render('trajet/list.html.twig', [
     'trajets' => $trajets,
     ]);
+    }
+
+    /**
+     * Créer un nouveau trajet.
+     * @Route("/nouveau-trajet", name="trajet.create")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse|Response
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response
+    {
+        $trajet = new Trajet();
+        $form = $this->createForm(TrajetType::class, $trajet);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($trajet);
+            $em->flush();
+            return $this->redirectToRoute('trajet.list');
+        }
+        return $this->render('trajet/create.html.twig', [
+        'form' => $form->createView(),
+        ]);
     }
 }
